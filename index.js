0eb2c811cfbd77f1603c0c971b24a8bb24ace26f
Ian W <iwsslkmpr@gmail.com>
2020-05-25 22:17:38 +0000

    Created basics of the express server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+// TODO: Adapt to dev/prod settings
+const port = 3000;
+
+app.use('/static', express.static(path.join(process.cwd(),"/build/static")));
+app.use(express.static(path.join(process.cwd(),"/StaticContent")));
+app.get('/', (req, res) => res.sendFile("./build/index.html", options={root:process.cwd()}))
+app.get('/carousel', (req, res) => res.sendFile("./build/index.html", options={root:process.cwd()}))
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
