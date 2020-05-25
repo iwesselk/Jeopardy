6d05766f63eaaf78b12b88e7d32da4da21a2607c
Ian W <iwsslkmpr@gmail.com>
2020-05-25 22:44:13 +0000

    Added JepAPI for file serving

---
+++ ExpressApp/JepAPI.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const fs = require('fs');
+const path = require('path');
+let app = express();
+
+// Test that it is working
+app.get('/', (req, res) => {
+    res.send("Hello from JepAPI");
+});
+
+// Actual Logic
+// TODO: Nothing here uses authentication. Has almost no verification
+app.get('/puzzles/:id', (req, res) => {
+    let jeppath = path.join(process.cwd(), "./Jepordy/"+req.param["id"]);
+    if (fs.existsSync(jeppath)) {
+        res.sendFile(jeppath);
+    } else {
+        res.json({success:false,errmsg="No File Found"});
+    }
+});
+
+app.put('/puzzles/:id', (req, res) => {
+    let jeppath = path.join(process.cwd(), "./Jepordy/"+req.param["id"]);
+    if (fs.existsSync(jeppath)) {
+        fs.unlinkSync(jeppath);
+    }
+    fs.writeFileSync(jeppath, req.body);
+});
+
+app.delete('/puzzles/:id', (req, res) => {
+    let jeppath = path.join(process.cwd(), "./Jepordy/"+req.param["id"]);
+    if (fs.existsSync(jeppath)) {
+        fs.unlinkSync(jeppath);
+        res.json({success:true,errmsg=""});
+    } else {
+        res.json({success:false},errmsg("File doesn't exist"));
+    }
+});
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
+const JepAPI = require('./ExpressApp/JepAPI');
 const app = express();
 // TODO: Adapt to dev/prod settings
 const port = 3000;
-
+app.use('/api/', JepAPI);
 app.use('/static', express.static(path.join(process.cwd(),"/build/static")));
 app.use(express.static(path.join(process.cwd(),"/StaticContent")));
 app.get('/', (req, res) => res.sendFile("./build/index.html", options={root:process.cwd()}))
